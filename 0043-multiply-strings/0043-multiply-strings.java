3f228ace8dc0f6c6ca29e097e1cec7ee28de2316
Deepak Gupta <deepakgupta090607@gmail.com>
2026-08-27 19:37:52 +0530

    Time: 2 ms (99.77%), Space: 43.8 MB (19.27%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        // If either number is 0
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+
+        int[] result = new int[m + n];
+
+        // Multiply digit by digit
+        for (int i = m - 1; i >= 0; i--) {
+
+            int digit1 = num1.charAt(i) - '0';
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                int digit2 = num2.charAt(j) - '0';
+
+                int product = digit1 * digit2;
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = product + result[p2];
+
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
+            }
+        }
+
+        // Convert result array to String
+        StringBuilder sb = new StringBuilder();
+
+        for (int digit : result) {
+            if (sb.length() == 0 && digit == 0) {
+                continue;
+            }
+
+            sb.append(digit);
+        }
+
+        return sb.toString();
+    }
+}
